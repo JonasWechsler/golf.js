11b24a4f050d916f87bdda0c67124c00a1a03119
JonasWechsler <jonaswechsler@gmail.com>
2017-11-13 10:06:00 -0600

    Removed duplicate files

--- src/debug.ts
+++
@@ -1,9 +0,0 @@
-class Logger{
-    public static debug(a){
-        console.log(a);
-    }
-
-    public static log(a){
-        console.log(a);
-    }
-}
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-class Tester{
-    run(){
-
-    }
-}
